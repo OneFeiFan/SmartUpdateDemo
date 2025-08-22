42e981c23bdae7c6aa19cc102b09d42c5596be05
OneFeiFan <3022504358@qq.com>
2025-08-22 10:21:45 +0800

    refactor: 移除不必要的导入并更新文本内容

--- demo/src/main/java/com/itheima/demo/MainActivity.java
+++ demo/src/main/java/com/itheima/demo/MainActivity.java
@@ -1,24 +1,19 @@
 package com.itheima.demo;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.MediaController;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import com.feifan.apkpatch.PatchUtils;
-
 
 import java.io.File;
 
